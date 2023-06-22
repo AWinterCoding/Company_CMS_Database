d49ecb9831d41b59cd64139c1ea655ef00c7a1a7
Alexandra <awinterdrova@gmail.com>
2023-06-22 13:26:05 -0600

    added architecture for seeds

---
+++ main/db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO departments(id, department)
+VALUES ();
+
+INSERT INTO roles(id, job_title, salary, department_id)
+VALUES ();
+
+INSERT INTO employees(id, first_name, last_name, role_id, manager_id)
+VALUES ();
