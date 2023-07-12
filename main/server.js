09d734159f38813744f2ac2662736c54a70edad5
Alexandra <awinterdrova@gmail.com>
2023-07-12 16:37:39 -0600

    added all selection functionality

--- main/server.js
+++ main/server.js
@@ -24,10 +24,16 @@ const db = mysql.createConnection(
 
 //inquirer menu
 async function init(){
+    quitCondition = false;
+    while(!quitCondition){quitCondition = await runMenu();}
+}
+
+async function runMenu(){
+    let menuCondition = false;
     const questionArray = [{
         name: "menu",
         type: "list",
-        message: "What would you like to do?",
+        message: "Please select what you would like to do?",
         choices: [
             "View all Departments",
             "View all Roles",
@@ -39,10 +45,51 @@ async function init(){
             "Quit"
         ]
     }];
-    const response = await inquirer.prompt(questionArray).then((answers) =>
-    {
-    console.log(answers);
-    });
+    await inquirer.prompt(questionArray).then((answers) =>{
+        console.log(answers.menu);
+    switch(answers.menu){
+        case "View all Departments":
+            db.query(`SELECT * FROM departments`, function(err, results){
+                console.log("\nHere are all of the Departments \n");
+                results.forEach(element => {
+                    console.log(element.department);
+                });
+            });
+            break;
+        case "View all Roles":
+            db.query(`SELECT * FROM roles`, function(err, results){
+                console.log("\nHere are all of the Roles\n");
+                results.forEach(element => {
+                    console.log(element.job_title);
+                });
+            });
+            break;
+        case "View all Employees":
+            db.query(`SELECT * FROM employees`, function(err, results){
+                console.log("\nHere are all of the Employees\n");
+                results.forEach(element => {
+                    console.log(`${element.first_name} ${element.last_name}`);
+                });
+            });
+            break;
+        case "Add a Department":
+            console.log("AddDepartment");
+            break;
+        case "Add a Role":
+            console.log("AddRole");
+            break;
+        case "Add an Employee":
+            console.log("AddEmployee");
+            break;
+        case "Update an Employee Role":
+            console.log("Update Employee");
+            break;
+        case "Quit":
+            return menuCondition = true;
+    }
+}
+    );
+    return menuCondition;
 }
 
 init();
