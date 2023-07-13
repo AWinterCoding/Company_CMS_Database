b93929e19aea08404f4c11a0878edaa535eff2ff
Alexandra <awinterdrova@gmail.com>
2023-07-13 15:28:45 -0600

    added base addition functions, going to expand functionality later

--- main/server.js
+++ main/server.js
@@ -23,11 +23,12 @@ const db = mysql.createConnection(
 );
 
 //inquirer menu
-async function init(){
+function init(){
     quitCondition = false;
-    while(!quitCondition){quitCondition = await runMenu();}
+    while(!quitCondition){quitCondition =  runMenu();}
 }
 
+//function to prompt the menu and move forward after making a selection
 async function runMenu(){
     let menuCondition = false;
     const questionArray = [{
@@ -46,40 +47,32 @@ async function runMenu(){
         ]
     }];
     await inquirer.prompt(questionArray).then((answers) =>{
-        console.log(answers.menu);
+        menuCondition = menuCheck(answers);
+        return menuCondition;
+    }
+    );
+}
+
+//function to check the answer of the prompt and then determining what process to complete
+function menuCheck(answers){
     switch(answers.menu){
         case "View all Departments":
-            db.query(`SELECT * FROM departments`, function(err, results){
-                console.log("\nHere are all of the Departments \n");
-                results.forEach(element => {
-                    console.log(element.department);
-                });
-            });
+            departmentFetch();
             break;
         case "View all Roles":
-            db.query(`SELECT * FROM roles`, function(err, results){
-                console.log("\nHere are all of the Roles\n");
-                results.forEach(element => {
-                    console.log(element.job_title);
-                });
-            });
+            roleFetch();
             break;
         case "View all Employees":
-            db.query(`SELECT * FROM employees`, function(err, results){
-                console.log("\nHere are all of the Employees\n");
-                results.forEach(element => {
-                    console.log(`${element.first_name} ${element.last_name}`);
-                });
-            });
+            employeeFetch();
             break;
         case "Add a Department":
-            console.log("AddDepartment");
+            departmentCreation();
             break;
         case "Add a Role":
-            console.log("AddRole");
+            roleCreation();
             break;
         case "Add an Employee":
-            console.log("AddEmployee");
+            employeeCreation();
             break;
         case "Update an Employee Role":
             console.log("Update Employee");
@@ -88,8 +81,79 @@ async function runMenu(){
             return menuCondition = true;
     }
 }
-    );
-    return menuCondition;
+
+function employeeFetch(){
+    db.query(`SELECT * FROM employees`, function(err, results){
+        console.log("\nHere are all of the Employees\n");
+        results.forEach(element => {
+            console.log(`${element.first_name} ${element.last_name}`);
+        });
+    });
 }
 
+//function that displays all current roles
+function roleFetch(){
+    db.query(`SELECT * FROM roles`, function(err, results){
+        console.log("\nHere are all of the Roles\n");
+        results.forEach(element => {
+            console.log(element.job_title);
+        });
+    });
+}
+
+//function that displays all current departments
+function departmentFetch(){
+    db.query(`SELECT * FROM departments`, function(err, results){
+        console.log("\nHere are all of the Departments \n");
+        results.forEach(element => {
+            console.log(element.department);
+        });
+    });
+}
+
+//function that creates a new department
+async function departmentCreation(){
+    const question = [{
+        name: "deptName",
+        type: "input",
+        message: "What would you like the new department to be called?",
+    }];
+    inquirer.prompt(question).then((answer)=>{
+  db.query(`INSERT INTO departments (department)
+  VALUES (?)`, answer.deptName, function(err, result){
+    departmentFetch();
+  });
+
+    })
+}
+
+//function to create a new  role.
+async function roleCreation(){
+    const question = [{
+        name: "roleName",
+        type: "input",
+        message: "What would you like the new Role to be called?",
+    }];
+    inquirer.prompt(question).then((answer)=>{
+  db.query(`INSERT INTO roles (job_title)
+  VALUES (?)`, answer.roleName, function(err, result){
+    roleFetch();
+  });
+    })
+}
+
+//function to create a new employee
+async function employeeCreation(){
+    const question = [{
+        name: "employeeName",
+        type: "input",
+        message: "What is the Employees first name?",
+    }];
+    inquirer.prompt(question).then((answer)=>{
+  db.query(`INSERT INTO employees (first_name)
+  VALUES (?)`, answer.employeeName, function(err, result){
+    employeeFetch();
+  });
+    })
+}
 init();
